71eded667eee9882b7df81dbfd91369fef61d328
Gabriel Estevam <gabriel.estevam.narciso@gmail.com>
2021-07-19 18:10:23 -0300

    Criação das configurações iniciais do projeto

---
+++ src/main/java/com/example/repositoriogcm/RepositorioGcmApplication.java
@@ -0,0 +1,13 @@
+package com.example.repositoriogcm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepositorioGcmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepositorioGcmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/repositoriogcm/RepositorioGcmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.repositoriogcm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepositorioGcmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
