4b3f840398f4800d6c756cac9a6cdd833ebb9607
Gabriel Estevam <gabriel.estevam.narciso@gmail.com>
2021-09-06 00:09:52 -0300

    test: Corrigir testes com problemas

--- backend/bancoVestido/src/test/java/br/ufrn/imd/bancovestido/conta/TestCreditoOperation.java
+++ backend/bancoVestido/src/test/java/br/ufrn/imd/bancovestido/conta/TestCreditoOperation.java
@@ -1,5 +1,6 @@
-package br.ufrn.imd.bancovestido;
+package br.ufrn.imd.bancovestido.conta;
 
+import br.ufrn.imd.bancovestido.BancoVestidoApplication;
 import br.ufrn.imd.bancovestido.enuns.TipoConta;
 import br.ufrn.imd.bancovestido.exception.InvalidValueException;
 import br.ufrn.imd.bancovestido.exception.ResourceNotFoundException;
@@ -7,18 +8,17 @@ import br.ufrn.imd.bancovestido.model.Conta;
 import br.ufrn.imd.bancovestido.model.Pessoa;
 import br.ufrn.imd.bancovestido.service.ContaService;
 import br.ufrn.imd.bancovestido.service.PessoaService;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.*;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.event.annotation.AfterTestClass;
+import org.springframework.test.context.event.annotation.BeforeTestClass;
 
 import java.math.BigDecimal;
 import java.util.Date;
 
-@SpringBootTest(classes = {BancoVestidoApplicationTests.class})
+@SpringBootTest(classes = {BancoVestidoApplication.class})
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class TestCreditoOperation {
 
@@ -31,56 +31,60 @@ public class TestCreditoOperation {
     private Conta conta;
     private Pessoa pessoa;
 
-    public TestCreditoOperation(){
-
-    }
-
     @BeforeEach
-    private void transferPreparation() throws ResourceNotFoundException, InvalidValueException {
+    public void setUp() throws ResourceNotFoundException, InvalidValueException {
         MockitoAnnotations.initMocks(this);
+
         Pessoa pessoa = new Pessoa();
         pessoa.setNome("André Ricardo");
         pessoa.setDataNascimento(new Date());
 
-        pessoa = this.pessoaService.save(pessoa);
-        this.pessoa = this.pessoaService.findOne(pessoa.getId());
+        this.pessoa = this.pessoaService.save(pessoa);
 
         Conta conta = new Conta();
-        conta.setPessoa(pessoa);
-        conta.setNumeroConta("1208");
+        conta.setPessoa(this.pessoa);
+        conta.setNumeroConta("1000");
         conta.setSaldo(new BigDecimal(6000));
 
-        conta = this.contaService.save(conta);
-        this.conta = this.contaService.findOne(conta.getNumeroConta());
+        this.conta  = this.contaService.save(conta);
+    }
+
+    @AfterEach
+    public void tearDown() throws ResourceNotFoundException {
+        this.contaService.delete(this.conta.getNumeroConta());
+        this.pessoaService.delete(this.pessoa.getId());
     }
 
     @Test
-    public void creditandoContaPoupanca() throws ResourceNotFoundException {
+    public void creditandoContaPoupanca() throws ResourceNotFoundException, InvalidValueException {
         BigDecimal valor = new BigDecimal(500);
         this.conta.setTipoConta(TipoConta.CONTA_POUPANCA);
+        this.contaService.save(this.conta);
         this.contaService.credito(this.conta.getNumeroConta(), valor, 100);
         this.conta = this.contaService.findOne(this.conta.getNumeroConta());
         Assertions.assertEquals(new BigDecimal(6500).doubleValue(), this.conta.getSaldo().doubleValue());
     }
 
     @Test
-    public void creditandoContaCorrente() throws ResourceNotFoundException {
+    public void creditandoContaCorrente() throws ResourceNotFoundException, InvalidValueException {
         BigDecimal valor = new BigDecimal(500);
         this.conta.setTipoConta(TipoConta.CONTA_CORRENTE);
+        this.contaService.save(this.conta);
         this.contaService.credito(this.conta.getNumeroConta(), valor, 100);
         this.conta = this.contaService.findOne(this.conta.getNumeroConta());
         Assertions.assertEquals(new BigDecimal(6500).doubleValue(), this.conta.getSaldo().doubleValue());
     }
 
     @Test
-    public void creditandoContaBonus() throws ResourceNotFoundException {
+    public void creditandoContaBonus() throws ResourceNotFoundException, InvalidValueException {
         BigDecimal valor = new BigDecimal(500);
         this.conta.setTipoConta(TipoConta.CONTA_BONUS);
         this.conta.setPontuacao(100);
+        this.contaService.save(this.conta);
         this.contaService.credito(this.conta.getNumeroConta(), valor, 100);
         this.conta = this.contaService.findOne(this.conta.getNumeroConta());
         Assertions.assertEquals(new BigDecimal(6500).doubleValue(), this.conta.getSaldo().doubleValue());
-        Assertions.assertEquals(200, this.conta.getPontuacao());
+        Assertions.assertEquals(105, this.conta.getPontuacao());
     }
 
 
--- backend/bancoVestido/src/test/java/br/ufrn/imd/bancovestido/conta/TestTransferenciaOperation.java
+++ backend/bancoVestido/src/test/java/br/ufrn/imd/bancovestido/conta/TestTransferenciaOperation.java
@@ -1,5 +1,6 @@
-package br.ufrn.imd.bancovestido;
+package br.ufrn.imd.bancovestido.conta;
 
+import br.ufrn.imd.bancovestido.BancoVestidoApplication;
 import br.ufrn.imd.bancovestido.enuns.TipoConta;
 import br.ufrn.imd.bancovestido.exception.InvalidValueException;
 import br.ufrn.imd.bancovestido.exception.ResourceNotFoundException;
@@ -7,10 +8,7 @@ import br.ufrn.imd.bancovestido.model.Conta;
 import br.ufrn.imd.bancovestido.model.Pessoa;
 import br.ufrn.imd.bancovestido.service.ContaService;
 import br.ufrn.imd.bancovestido.service.PessoaService;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.*;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,9 +16,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.math.BigDecimal;
 import java.util.Date;
 
-@SpringBootTest(classes = {BancoVestidoApplicationTests.class})
+@SpringBootTest(classes = {BancoVestidoApplication.class})
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-
 class TestTransferenciaOperation {
     @Autowired
     private ContaService contaService;
@@ -36,48 +33,51 @@ class TestTransferenciaOperation {
 
     private Pessoa pessoaFinal;
 
-    public TestTransferenciaOperation(){
+    @AfterEach
+    public void tearDown() throws ResourceNotFoundException {
+        this.contaService.delete(this.contaFinal.getNumeroConta());
+        this.pessoaService.delete(this.pessoaFinal.getId());
+        this.contaService.delete(this.contaInicio.getNumeroConta());
+        this.pessoaService.delete(this.pessoaInicio.getId());
 
     }
 
     @BeforeEach
-    private void transferPreparation() throws ResourceNotFoundException, InvalidValueException {
+    public void setUp() throws ResourceNotFoundException, InvalidValueException {
         MockitoAnnotations.initMocks(this);
         Pessoa pessoaInicio = new Pessoa();
         pessoaInicio.setNome("André Ricardo");
         pessoaInicio.setDataNascimento(new Date());
 
-        pessoaInicio = this.pessoaService.save(pessoaInicio);
-        this.pessoaInicio = this.pessoaService.findOne(pessoaInicio.getId());
+        this.pessoaInicio = this.pessoaService.save(pessoaInicio);
 
         Conta contaInicio = new Conta();
-        contaInicio.setPessoa(pessoaInicio);
-        contaInicio.setNumeroConta("1208");
+        contaInicio.setPessoa(this.pessoaInicio);
+        contaInicio.setNumeroConta("1001");
         contaInicio.setSaldo(new BigDecimal(6000));
 
-        contaInicio = this.contaService.save(contaInicio);
-        this.contaInicio = this.contaService.findOne(contaInicio.getNumeroConta());
+        this.contaInicio = this.contaService.save(contaInicio);
 
         Pessoa pessoaFinal = new Pessoa();
         pessoaFinal.setNome("Gabriel Estevam");
         pessoaFinal.setDataNascimento(new Date());
 
-        pessoaFinal = this.pessoaService.save(pessoaFinal);
-        this.pessoaFinal = this.pessoaService.findOne(pessoaFinal.getId());
+        this.pessoaFinal = this.pessoaService.save(pessoaFinal);
 
         Conta contaFinal = new Conta();
-        contaFinal.setPessoa(pessoaFinal);
-        contaFinal.setNumeroConta("1712");
+        contaFinal.setPessoa(this.pessoaFinal);
+        contaFinal.setNumeroConta("1002");
         contaFinal.setSaldo(new BigDecimal(4000));
 
-        contaFinal = this.contaService.save(contaFinal);
-        this.contaFinal = this.contaService.findOne(contaFinal.getNumeroConta());
+        this.contaFinal = this.contaService.save(contaFinal);
     }
 
     @Test
     public void transferindoValorCorretoContaPoupanca() throws ResourceNotFoundException, InvalidValueException {
         BigDecimal valor = new BigDecimal(400);
         this.contaInicio.setTipoConta(TipoConta.CONTA_POUPANCA);
+        this.contaService.save(this.contaInicio);
+
         this.contaService.transferencia(this.contaInicio.getNumeroConta(), this.contaFinal.getNumeroConta(), valor);
 
         this.contaInicio = this.contaService.findOne(this.contaInicio.getNumeroConta());
@@ -94,6 +94,8 @@ class TestTransferenciaOperation {
     public void transferindoValorCorretoContaCorrente() throws ResourceNotFoundException, InvalidValueException {
         BigDecimal valor = new BigDecimal(400);
         this.contaInicio.setTipoConta(TipoConta.CONTA_CORRENTE);
+        this.contaService.save(this.contaInicio);
+
         this.contaService.transferencia(this.contaInicio.getNumeroConta(), this.contaFinal.getNumeroConta(), valor);
 
         this.contaInicio = this.contaService.findOne(this.contaInicio.getNumeroConta());
@@ -110,6 +112,8 @@ class TestTransferenciaOperation {
     public void transferindoValorCorretoContaBonus() throws ResourceNotFoundException, InvalidValueException {
         BigDecimal valor = new BigDecimal(400);
         this.contaInicio.setTipoConta(TipoConta.CONTA_BONUS);
+        this.contaService.save(this.contaInicio);
+
         this.contaService.transferencia(this.contaInicio.getNumeroConta(), this.contaFinal.getNumeroConta(), valor);
 
         this.contaInicio = this.contaService.findOne(this.contaInicio.getNumeroConta());
